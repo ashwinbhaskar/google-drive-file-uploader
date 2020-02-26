58219eb77d54f42e8a56c5953da81bb5d5ecc433
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2020-02-26 10:48:15 +0530

    Add more tests

--- test/google_drive_file_uploader/drive_test.clj
+++ test/google_drive_file_uploader/drive_test.clj
@@ -28,4 +28,34 @@
       (is (= ["foo-access-token"]
              (m/last-call #'drive/valid-access-token?)))
       (is (= ["foo-id-1" "/users/foo/a.apk" "foo-name.apk" "foo-access-token"]
+             (m/last-call #'drive/upload-file-multipart)))))
+  (testing "Should fetch a new access token if the supplied access-token is not valid"
+    (m/with-mock [drive/valid-access-token?   false
+                  drive/get-files             {:files [{:id "foo-id-1"
+                                                        :name "foo-folder"
+                                                        :mime-type "application/vnd.google-apps.folder"}]}
+                  drive/upload-file-multipart true
+                  drive/authorization-token "new-access-token"]
+      (is (drive/upload-file-to-folder {:folder "foo-folder"
+                                        :file-path "/users/foo/a.apk"
+                                        :file-name "foo-name.apk"
+                                        :access-token "foo-access-token"
+                                        :refresh-token "foo-refresh-token"
+                                        :client-id "foo-client-id"
+                                        :client-secret "foo-client-secret"}))
+      (is (= 1
+             (m/call-count #'drive/valid-access-token?)))
+      (is (= 1
+             (m/call-count #'drive/get-files)))
+      (is (= 1
+             (m/call-count #'drive/upload-file-multipart)))
+      (is (= 1
+             (m/call-count #'drive/authorization-token)))
+      (is (= ["foo-access-token"]
+             (m/last-call #'drive/valid-access-token?)))
+      (is (= ["foo-access-token"]
+             (m/last-call #'drive/valid-access-token?)))
+      (is (= ["foo-refresh-token" "foo-client-id" "foo-client-secret"]
+             (m/last-call #'drive/authorization-token)))
+      (is (= ["foo-id-1" "/users/foo/a.apk" "foo-name.apk" "new-access-token"]
              (m/last-call #'drive/upload-file-multipart))))))
